fe5450a7a9e086f6cae0eef87755ac41d3121dbf
KTAN119 <44692330+KTAN119@users.noreply.github.com>
2021-10-25 21:52:09 +0800

    added showbooking

--- showbooking.php
+++ showbooking.php
@@ -13,18 +13,87 @@ if (!$conn) {
 // if(isset($_GET['movie_id'])) {
 //   echo $_GET['movie_id'];
 //  }
-$Email = $_POST['email'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
-$sql = "SELECT * FROM Movie WHERE ID = $ID";
+$sql = "SELECT * FROM Movie_sales WHERE Email = '$email'";
 
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-$Name = $row['Name'];
-$Image_path = $row['Image_path'];
-$Length = $row['Length'];
-$Language = $row['Language'];
-$Casting = $row['Casting'];
-$Rating = $row['Rating'];
+// echo '<!DOCTYPE html>';
+// echo '<html lang="en">';
+// echo '<head>';
+// echo '<title>HomePage</title>';
+// echo '<link rel = "stylesheet" href = 'background.css'>';
+// echo '<style>';
+// echo '.table{width: 100%;}';
+// echo '.table td {text-align: center; padding: 15px;}';         
+// echo '</style> ';
+// echo '</head>';
+// echo '</html>';
+
+if (mysqli_query($conn, $sql)){
+    $result = $conn->query($sql);
+    $num_result = $result->num_rows;
+    echo "<style>";
+    echo "table,th,td { border: 1px solid white; }";
+    echo "</style>";
+    echo "<table>";
+    echo "<tr>";
+    echo "<th> Movie </th>";
+    echo "<th> Date </th>";
+    echo "<th> Time </th>";
+    echo "<th> Seat </th>";
+    echo "</tr>";
+    for ($i = 0; $i<$num_result; $i++){
+        $row = $result->fetch_assoc();
+        if ($row['Phone'] == $phone){
+            $movie_id = $row['Movie'];
+            $sql2 = "SELECT * FROM Movie WHERE ID = $movie_id";
+            $result2 = $conn->query($sql2);
+            $row2 = $result2->fetch_assoc();
+            $movie = $row2['Name'];
+            echo "<tr>";
+            echo "<td>".$movie."</td>";
+            echo "<td>".$row['Date']."</td>";
+            echo "<td>".$row['Time']."</td>";
+            echo "<td>".$row['Seat']."</td>"; 
+            echo "</tr>";
+        }
+    }}
+    else{
+        echo mysqli_error($conn);
+    }
 
 mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>HomePage</title>
+    <link rel = "stylesheet" href = 'background.css'>
+    <style>
+      .table{width: 100%;}
+      .table td {text-align: center;
+           padding: 15px;}
+    </style>  
+  </head>
+  <body>
+    <header>
+      <a href = "index.html"><img src="logo.jpg" alt="Logo"/></a>
+      <a href="login.html"><p id="login">Login/Sign Up</p></a>
+    </div>
+    </header>
+    <div class="topnav">
+      <a href="nowshowing.html">Now Showing</a>
+      <a href="comingsoon.html">Coming Soon</a>
+      <a href="promotion.html">Promotion</a>
+      <a href="checkbooking.html">Show My Bookings</a>
+    </div>
+    <h1><strong>Your Booking</strong></h1>
+    <div class = "movie_poster">
+      <table class = "table">
+      </table>
+    </div>
+  </body>
+  <footer></footer>
+</html>
